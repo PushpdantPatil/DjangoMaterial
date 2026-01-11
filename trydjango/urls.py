54fb74349b791d449c67ae400600eb2282d6bacd
Pushpdant <pushpdantpatil14@gmail.com>
2026-01-11 22:45:44 +0530

    Added Django app and views for same

--- trydjango/urls.py
+++ trydjango/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for trydjango project.
+"""trydjango URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,11 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 
-from .views import *
+from articles import views
+from .views import home_view
 
 urlpatterns = [
-    path('', home),
+    path('', home_view), # index / home / root
+    path('articles/<int:id>/', views.article_detail_view),
+    # re_path(r'articles/(?P<id>\d+)/$', home_view),
     path('admin/', admin.site.urls),
 ]
